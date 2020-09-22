8e3c82f8f7d2fc9c16f38e32327787db391780b3
esneidermanzano <esneidermanzano@gmail.com>
2020-09-21 21:28:33 -0500

    fix: tenant error

---
+++ Backend/rest/app_tenant/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppTenantConfig(AppConfig):
+    name = 'app_tenant'
--- Backend/rest/test.py
+++ Backend/rest/test.py
@@ -1,10 +1,9 @@
 from app_tenant.models import AppTenant
 
-
 # create your public tenant
 tenant = AppTenant(domain_url="stemen.localhost",
     schema_name='stemen',
-                name='loquendo manzano.',
+                name='stemen.',
                 paid_until='2020-12-05',
                 on_trial=False)
 tenant.save()
