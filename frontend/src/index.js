415513bf3485dd6876f43262d34189ce09390360
Valle1806 <vallecilla.cristian@correounivalle.edu.co>
2020-10-10 03:24:47 -0500

    feat: landing page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import { Router, Route, Switch, Redirect } from "react-router-dom";
 
 import AdminLayout from "layouts/Admin.js";
 import Login from "layouts/Login.js";
+import LandingPage from "layouts/LandingPage.js"
 
 import "assets/scss/black-dashboard-react.scss";
 import "assets/css/nucleo-icons.css";
@@ -15,6 +16,8 @@ ReactDOM.render(
   <Router history={hist}>
     <Switch>
       <Route exact path="/login" component={() => <Login />} />
+      <Route  path="/request/:id" component={()=><LandingPage />}/>
+      <Route path="/landing" component={()=><LandingPage />}/>
       <Route path="/admin" component={(props) => <AdminLayout {...props} />} />
       <Redirect from="/" to="/admin/users" />
     </Switch>
