bf245f35b99afed10f6be9adc873362a09fe1b04
Valle1806 <vallecilla.cristian@correounivalle.edu.co>
2020-10-12 04:12:05 -0500

    feat: rental request and create tenant

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,6 +8,7 @@ import store from "./redux/store.js";
 import Admin from "layouts/Admin.js";
 import Login from "layouts/Login.js";
 import LandingPage from "layouts/LandingPage.js"
+import Suscription from "layouts/Suscription.js"
 
 import "assets/scss/black-dashboard-react.scss";
 import "assets/css/nucleo-icons.css";
@@ -19,8 +20,8 @@ ReactDOM.render(
     <Router history={history}>
       <Switch>
         <Route exact path="/login" component={Login} />
-        <Route  path="/request/:id" component={()=><LandingPage />}/>
-        <Route path="/landing" component={()=><LandingPage />}/>
+        <Route  path="/request/:id" component={Suscription}/>
+        <Route path="/landing" component={LandingPage}/>
         <Route path="/admin" component={Admin} />
         <Redirect from="/" to="/admin/users" />
       </Switch>
