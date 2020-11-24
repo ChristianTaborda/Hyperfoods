2878a692664a287854d74bcf79724044c6ca275d
Christian Taborda <christian.taborda@correounivalle.edu.co>
2020-11-24 13:23:00 -0500

    Report 2
    - Add View for Report 2

--- backend/rest/reports/views.py
+++ backend/rest/reports/views.py
@@ -1,8 +1,10 @@
 from rest_framework.views import View
 from django.http import HttpResponse
 from invoiceDetails.models import InvoiceDetail
+from invoices.models import Invoice
 from products.models import Product
 from products.serializers import ProductSerializer
+from invoices.serializers import InvoiceSerializer
 import json
 
 # View for Report 1:
@@ -33,24 +35,27 @@ class MostSelledProducts(View):
 
         return HttpResponse(json.dumps(response))
 
-"""
 # View for Report 2:
 class HoursWithMoreSales(View):
     def get(self, request):
 
         # Hour sales count:
-        hours = 
-        dictionary = []
-        for i in range(len(products)):
-            product = products[i]['codeProduct']
+        times = Invoice.objects.all().values('dateTimeInvoice')
+        hours = []
+        for i in times:
+            hour = i['dateTimeInvoice'].strftime('%H')
+            if (hour not in hours):
+                hours.append(hour)
 
-            invoiceDetails = InvoiceDetail.objects.filter(productInvoiceDetail = product)
+        dictionary = []
+        for i in hours:
             sales = 0
-            for j in invoiceDetails:
-                sales = sales + j.quantityInvoiceDetail
+            for j in times:
+                if (i == j['dateTimeInvoice'].strftime('%H')):
+                    sales = sales + 1
 
             data = {
-                'product': products[i],
+                'time': i,
                 'sales': sales                     
             }
             dictionary.append(data)
@@ -61,6 +66,6 @@ class HoursWithMoreSales(View):
         }
 
         return HttpResponse(json.dumps(response))
-"""
+
 
 
