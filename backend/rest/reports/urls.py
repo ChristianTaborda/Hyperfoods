c08da125b205bec3f8dd07dddced14386e650fd7
Christian Taborda <christian.taborda@correounivalle.edu.co>
2020-11-24 15:44:10 -0500

    Report 4
    - Add View for Report 4

--- backend/rest/reports/urls.py
+++ backend/rest/reports/urls.py
@@ -3,12 +3,14 @@ from django.urls import path
 from .views import (
     MostSelledProducts,
     HoursWithMoreSales,
-    ClientsWithMorePurchases
+    ClientsWithMorePurchases,
+    ZonesWithMorePurchases
 )
 
 # URLS for reports:
 urlpatterns = [
     path('1/', MostSelledProducts.as_view()),
     path('2/', HoursWithMoreSales.as_view()),
-    path('3/', ClientsWithMorePurchases.as_view())
+    path('3/', ClientsWithMorePurchases.as_view()),
+    path('4/', ZonesWithMorePurchases.as_view())
 ]
